18a100cd2f4fad9713d5ec25a7d0ec947c8e47f1
teslali <15736818863@163.com>
2019-05-04 15:23:16 +0800

    add

---
+++ src/translate.cpp
@@ -0,0 +1,21 @@
+/*************************************************************************
+	> File Name: translate.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2019年04月30日 星期二 23时26分22秒
+ ************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define NUMCNT 256
+#define BASE 128
+
+
+
+int main() {
+
+
+    return 0;
+}
